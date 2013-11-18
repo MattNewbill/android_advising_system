30b9fedcdecbfc521b4df88490b81744a8b2dea6
Matt Martineau <mmartineau31@gmail.com>
2013-11-17 23:40:32 -0800

    Added a list view for a few of our main activities. Everything currently links to a blank page. MainActivity.class currently has nothing to do with the project. It is just the default hello world.

---
+++ Advisement_System/src/com/goldteam/advisement_system/SemesterCalendar.java
@@ -0,0 +1,15 @@
+package com.goldteam.advisement_system;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SemesterCalendar extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.semester_calendar);
+	}
+	
+}
