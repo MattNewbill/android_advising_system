da738c1a3ee6bfe51ef35e729e80cd275b5821f3
Jeff Limbacher <jeffreylimbacher@gmail.com>
2013-11-27 21:07:02 -0800

    Added comments and Javadocs

--- Advisement_System/src/com/goldteam/advisement_system/CourseExpandableListAdapter.java
+++ Advisement_System/src/com/goldteam/advisement_system/CourseExpandableListAdapter.java
@@ -13,19 +13,33 @@ import android.widget.BaseExpandableListAdapter;
 import android.widget.CheckedTextView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+/**
+ * This class is used to put the requirements and courses into the expandable list.
+ * The expandable list looks like this:
+ * <p>
+ * Requirement<br>
+ * &nbsp;Course<br>
+ * &nbsp;Course<br>
+ * Requirement<br>
+ * &nbsp;Course<br>
+ * etc.
+ * <p>
+ * The courses are initially hidden until the requirement is clicked which exposes
+ * the list of requirements.
+ * 
+ * @author Jeffrey Limbacher
+ */
 public class CourseExpandableListAdapter extends BaseExpandableListAdapter {
 	
+	
+	//All the requirements to be in the list
 	private ArrayList<Requirement> reqs;
 	//The position of the ArrayList corresponds to the requirements that the arraylist is associated to
-	private ArrayList<ArrayList<Course>> courses; 
-	private Course tempCourse;
-	private Context con;
+	private ArrayList<ArrayList<Course>> courses;
+	//Used to expand the Views in the List.
 	private LayoutInflater inflater;
-	private Activity activity;
 	
 	public CourseExpandableListAdapter(Context context, ArrayList<Requirement> requirements) {
-		this.con = context;
 		this.inflater = LayoutInflater.from(context);
 		this.reqs = requirements;
 		courses = new ArrayList<ArrayList<Course>>();
@@ -33,7 +47,10 @@ public class CourseExpandableListAdapter extends BaseExpandableListAdapter {
 		//This implementation assumes unique courses for now.
 		for(int i = 0; i < reqs.size(); i++){
 			Requirement req = reqs.get(i);
+			//Keep the number of ArrayList<Course>s in sync with Requirements.
 			courses.add(new ArrayList<Course>());
+			//The subrequirements each contain different courses
+			//Insert all the courses into the expandable lists under the requirements.
 			for(SubRequirement subReq : req.getSubRequirements()) {
 				ArrayList<Course> subReqCourses = subReq.getCourses();
 				courses.get(i).addAll(subReqCourses);
@@ -41,6 +58,12 @@ public class CourseExpandableListAdapter extends BaseExpandableListAdapter {
 		}
 	}
 	
+	/**
+	 * Adds a requirement to the list. The requirement should have all its subrequirements already.
+	 * However, the method does not check if any subrequirements exist.
+	 * 
+	 * @param req A requirement which contains all its required courses.
+	 */
 	public void addRequirement(Requirement req) {
 		reqs.add(req);
 		int reqPosition = reqs.size() - 1;
@@ -51,14 +74,22 @@ public class CourseExpandableListAdapter extends BaseExpandableListAdapter {
 		}
 	}
 	
+	/**
+	 * Adds a course to the specified requirement.
+	 * @param reqPos The position of the requirement.
+	 * @param course The course to add.
+	 */
 	public void addCourse(int reqPos, Course course){
 		ArrayList<Course> reqCourses = courses.get(reqPos);
 		reqCourses.add(course);
 	}
 	
-	public void setInflater(LayoutInflater mInflater, Activity act) {
+	/**
+	 * Sets the LayoutInflater.
+	 * @param mInflater The inflater to be set to.
+	 */
+	public void setInflater(LayoutInflater mInflater) {
 		this.inflater = mInflater;
-		activity = act;
 	}
 
 	@Override
@@ -77,25 +108,20 @@ public class CourseExpandableListAdapter extends BaseExpandableListAdapter {
 	@Override
 	public View getChildView(int groupPosition, int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
-		Log.d("CourseExpandableListAdapater", "getChildView");
-		Log.d("CourseExpandableListAdapter", "childPosition: " + String.valueOf(childPosition));
-		Log.d("CourseExpandableListAdapter", "Children length: " + String.valueOf(courses.size()));
+		//Retrieve the child requested
 		ArrayList<Course> tempChild = courses.get(groupPosition);
-		Log.d("CourseExpandableListAdapter", "tempChild size: " + String.valueOf(tempChild.size()));
-		if (convertView == null) {
+		Course tempCourse = tempChild.get(childPosition);
+		//Put the children in textViews
+		if(convertView == null) {
 			convertView = inflater.inflate(android.R.layout.simple_list_item_1, null);
 		}
-		Log.d("CourseExpandableAdapter", "covertView class is " + convertView.getClass().toString());
-		//Put the children in textViews.
-		View view = inflater.inflate(android.R.layout.simple_list_item_1, null);
-		Log.d("CourseExpandableListAdapter", "Class is " + view.getClass().toString());
-		tempCourse = tempChild.get(childPosition);
-		Log.d("CourseExpandableListAdapter", "Checkpoint");
 		String courseName = tempCourse.getName();
-		((TextView)convertView).setText(courseName);
+		((TextView) convertView).setText(courseName);
+		//Set as tag for click callback to retrieve the object
 		convertView.setTag(tempCourse);
 		convertView.setOnClickListener(new OnClickListener() {
 		   @Override
+		   //Listener just logs the name of the string to LogCat
 		public void onClick(View v) {
 			   Course course = (Course) v.getTag();
 			   Log.d("CourseExpandableListAdapter", course.toString());
@@ -129,12 +155,11 @@ public class CourseExpandableListAdapter extends BaseExpandableListAdapter {
 	@Override
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
-		Log.d("CourseExpandableListAdapater", "getGroupView");
+		//Make convertView be a simple_list_item
 		if (convertView == null) {
 		   convertView = inflater.inflate(android.R.layout.simple_list_item_1, null);
 		}
 		((TextView) convertView).setText(reqs.get(groupPosition).getName());
-		//((CheckedTextView) convertView).setChecked(isExpanded);
 		return convertView;
 	}
 
