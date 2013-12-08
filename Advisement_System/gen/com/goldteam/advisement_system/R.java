749bab256f3a3c98e3fde773c83648dcdb6507ae
Matt Newbill <matt@bitworkssystems.com>
2013-12-08 14:15:47 -0800

    Added Forums view

--- Advisement_System/gen/com/goldteam/advisement_system/R.java
+++ Advisement_System/gen/com/goldteam/advisement_system/R.java
@@ -24,15 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f080004;
+        public static final int TabHost1=0x7f080000;
+        public static final int action_settings=0x7f080010;
+        public static final int author=0x7f08000d;
         public static final int LinearLayout1=0x7f080000;
         public static final int action_settings=0x7f08000a;
         public static final int button1=0x7f080002;
         public static final int buttonPlannerDisplay=0x7f080005;
         public static final int course_selector_checkedtextview=0x7f080007;
         public static final int course_selector_textview=0x7f080008;
+        public static final int date=0x7f08000c;
         public static final int expandableListView1=0x7f080003;
         public static final int listView1=0x7f080004;
+        public static final int listView2=0x7f08000e;
+        public static final int numberOfPosts=0x7f08000b;
+        public static final int question=0x7f08000a;
         public static final int textView1=0x7f080001;
+        public static final int textViewPlannerDisplay=0x7f080007;
+        public static final int webview=0x7f08000f;
         public static final int textViewPlannerDisplay=0x7f080006;
         public static final int webview=0x7f080009;
     }
@@ -43,8 +53,9 @@ public final class R {
         public static final int class_planner=0x7f030003;
         public static final int course_selector_checkedtextview=0x7f030004;
         public static final int course_selector_textview=0x7f030005;
-        public static final int forums=0x7f030006;
-        public static final int semester_calendar=0x7f030007;
+        public static final int forum_listview_row=0x7f030006;
+        public static final int forums=0x7f030007;
+        public static final int semester_calendar=0x7f030008;
     }
     public static final class menu {
         public static final int class_selector=0x7f070000;
@@ -53,7 +64,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int author=0x7f050008;
+        public static final int date=0x7f050009;
         public static final int hello_world=0x7f050002;
+        public static final int numberOfPosts=0x7f05000a;
+        public static final int question=0x7f050007;
         public static final int select_courses=0x7f050005;
         public static final int select_major=0x7f050003;
         public static final int submit_courses=0x7f050006;
--- Advisement_System/src/com/goldteam/advisement_system/Forums.java
+++ Advisement_System/src/com/goldteam/advisement_system/Forums.java
@@ -1,15 +1,23 @@
 package com.goldteam.advisement_system;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class Forums extends Activity{
+public class Forums extends ListActivity{
+	
+	String posts[] = {"Question1", "Question2", "Question3"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.forums);
+		setListAdapter(new ArrayAdapter<String>(Forums.this, android.R.layout.simple_list_item_1, posts));	
 	}
-
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+	}
+	
 }
