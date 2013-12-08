c4327f7ac6018e761669b1151f62c3271ce97b3b
Matt Newbill <matt@bitworkssystems.com>
2013-12-08 14:40:28 -0800

    Merged mine R.java changes with Master

--- Advisement_System/gen/com/goldteam/advisement_system/R.java
+++ Advisement_System/gen/com/goldteam/advisement_system/R.java
@@ -24,27 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080004;
-        public static final int TabHost1=0x7f080000;
-        public static final int action_settings=0x7f080010;
-        public static final int author=0x7f08000d;
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f08000f;
+        public static final int author=0x7f08000c;
         public static final int button1=0x7f080002;
         public static final int buttonPlannerDisplay=0x7f080005;
         public static final int course_selector_checkedtextview=0x7f080007;
         public static final int course_selector_textview=0x7f080008;
-        public static final int date=0x7f08000c;
+        public static final int date=0x7f08000b;
         public static final int expandableListView1=0x7f080003;
         public static final int listView1=0x7f080004;
-        public static final int listView2=0x7f08000e;
-        public static final int numberOfPosts=0x7f08000b;
-        public static final int question=0x7f08000a;
+        public static final int listView2=0x7f08000d;
+        public static final int numberOfPosts=0x7f08000a;
+        public static final int question=0x7f080009;
         public static final int textView1=0x7f080001;
-        public static final int textViewPlannerDisplay=0x7f080007;
-        public static final int webview=0x7f08000f;
         public static final int textViewPlannerDisplay=0x7f080006;
-        public static final int webview=0x7f080009;
+        public static final int webview=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_course_selector=0x7f030000;
